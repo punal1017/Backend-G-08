8610479b6d2563ec544cd45b5d7c9038240717ea
xyz <punal690@gmail.com>
2024-08-29 09:55:18 +0530

    hello

---
+++ new.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        name: "Punal",
+        age: 1,
+        section: "b"
+    },
+    {
+        name: "Aman",
+        age: 2,
+        section: "a"
+    },
+    {
+        name: "Riya",
+        age: 3,
+        section: "c"
+    },
+    {
+        name: "Sahil",
+        age: 4,
+        section: "b"
+    },
+    {
+        name: "Neha",
+        age: 5,
+        section: "a"
+    }
+];
